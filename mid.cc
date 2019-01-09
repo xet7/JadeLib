0a12f58169edd757bc1f9e4f99e458a30234ff27
Armando Rivera <airrsongs@gmail.com>
2019-01-09 12:31:35 -0500

    Added guard if start parameter is zero

--- mid.cc
+++ mid.cc
@@ -4,6 +4,7 @@ FUNCTION STRING MID$ (STRING s, INT start, INT length) DO
   IF(s.length() < start) THEN
     RETURN "";
   END
+  IF(start == 0) start = 1;
   RETURN s.substr(start-1,length);
 END
 
