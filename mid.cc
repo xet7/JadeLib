64687534f87b027d1b6b11659316218fb9910771
Armando Rivera <airrsongs@gmail.com>
2019-01-08 20:39:49 -0500

    Added check for string length and start parameter

--- mid.cc
+++ mid.cc
@@ -1,6 +1,9 @@
 #include "include/jade.hpp"
 
 FUNCTION STRING MID$ (STRING s, INT start, INT length) DO
+  IF(s.length() < start) THEN
+    RETURN "";
+  END
   RETURN s.substr(start,length);
 END
 
