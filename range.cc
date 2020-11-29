d75f1d09970295e033406252fe5198e6b52c41ba
Armando Rivera <airr@outlook.com>
2020-11-28 20:19:40 -0500

    Added error handling for out-of-range condition

--- range.cc
+++ range.cc
@@ -1,7 +1,14 @@
 #include "include/jade.hpp"
 
 FUNCTION ARRAY<long> RANGE(long low, long high) {
-    ARRAY<long> v(high);
-    std::iota (std::begin(v), std::end(v), 0);
+    IF (low > high) THEN
+        ABORT("Out of Bounds error in RANGE() function");
+    END
+
+    IF (low > 0) high+=1;
+
+    long array_size = high - low;
+    ARRAY<long> v(array_size);
+    std::iota (std::begin(v), std::end(v), low);
     return v;
 }
